f2ace53fc183244dc8bcd176b55f5f21e1690be6
Paulo Adson <paulo.alves200581@gmail.com>
2024-05-07 00:26:00 -0300

    aprendendo mais sobre tipos e variáveis

--- java-anatomia-classes/src/estudo/paulo/tiposvariaveis/tiposvariaveis.java
+++ java-anatomia-classes/src/estudo/paulo/tiposvariaveis/tiposvariaveis.java
@@ -1,5 +1,12 @@
 package estudo.paulo.tiposvariaveis;
 
 public class tiposvariaveis {
-    
+    public static void main(String[] args) throws Exception {
+        double salarioMinimo = 2500;
+        System.out.println(salarioMinimo);
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+    }
 }
