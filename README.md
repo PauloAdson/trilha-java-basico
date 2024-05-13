d2699f8128e5c0a8db086c119e14026e4b81127c
Paulo Adson <paulo.alves200581@gmail.com>
2024-05-13 13:39:02 -0300

    constantes e variaveis

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # trilha-java-basico
 Estudo de Java Bootcamp DIO
+### O que estudar?
+<ul>
+    <li>Tipos Primitivos</li>
+    <li>Classe String</li>
+</ul>
---
+++ java-anatomia-classes/src/estudo/paulo/tiposvariaveis/constantesEVariaveis.java
@@ -0,0 +1,13 @@
+package estudo.paulo.tiposvariaveis;
+
+public class constantesEVariaveis {
+    public static void main(String[] args) {
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+
+        // Uma constante deve ser escrita de caixa alta, e utilizamos o final, para não alterar nada na constante
+        final double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI);
+    }
+}
