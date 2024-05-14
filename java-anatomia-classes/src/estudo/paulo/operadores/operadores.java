03fd5898f258650c3f2402a3de5755a740edd07f
Paulo Adson <paulo.alves200581@gmail.com>
2024-05-13 22:03:50 -0300

    aprendendo sobre operadores

---
+++ java-anatomia-classes/src/estudo/paulo/operadores/operadores.java
@@ -0,0 +1,10 @@
+package estudo.paulo.operadores;
+
+public class operadores {
+    public static void main(String[] args) {
+        String primeiroNome = "Paulo ";
+        String ultimoNome = "Filho";
+        String nomeCompleto = primeiroNome + ultimoNome;
+        System.out.println(nomeCompleto);
+    }
+}
