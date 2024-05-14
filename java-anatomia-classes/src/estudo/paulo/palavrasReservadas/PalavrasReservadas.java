7ed8356462d0e1c6e3a8eb7f9d952ded78653f8c
Paulo Adson <paulo.alves200581@gmail.com>
2024-05-14 18:39:12 -0300

    palavras reservadas

---
+++ java-anatomia-classes/src/estudo/paulo/palavrasReservadas/PalavrasReservadas.java
@@ -0,0 +1,17 @@
+package estudo.paulo.palavrasReservadas;
+
+public class PalavrasReservadas {
+    // Controle de pacotes ex: import, package
+
+    // Modificadores de acesso: public, private e protected
+
+    // Primitivos: boolean, byte, char, double, float, int, long, short e void
+
+    /*
+     * Modificadores de classes: abstract, class, extends, final, implements,
+     * interface, native, new, static, strictfp, synchronized, transient e volatile,
+     */
+
+    // Controle de fluxo dentro de um bloco de código: break, case e continue...
+    // ainda tem muitas outras palavras reservadas
+}
