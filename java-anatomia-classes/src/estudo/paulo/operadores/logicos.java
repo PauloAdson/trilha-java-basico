aff9a4c9df022b19ba22ae87e9eb28bd98d9397a
Paulo Adson <paulo.alves200581@gmail.com>
2024-05-13 23:41:57 -0300

    aprendendo sobre operadores lógicos

---
+++ java-anatomia-classes/src/estudo/paulo/operadores/logicos.java
@@ -0,0 +1,24 @@
+package estudo.paulo.operadores;
+
+// tabela verdade
+public class logicos {
+    public static void main(String[] args) {
+        boolean condicao1 = true;
+        boolean condicao2 = true;
+
+        // && E
+        if (condicao1 && condicao2) {
+            System.out.println("As duas condições são verdadeiras");
+        }
+
+        if (condicao1 && (7 > 6)) {
+            System.out.println("As duas condições são verdadeiras");
+        }
+        
+        // || OU
+        if (condicao1 || condicao2) {
+            System.out.println("Uma das condições são verdadeiras");
+        }
+        System.out.println("FIM!");
+    }
+}
