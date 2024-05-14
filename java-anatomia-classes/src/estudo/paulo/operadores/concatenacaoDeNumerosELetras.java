2ebe64eb553519d66d323463f55b21e10bd0a3ee
Paulo Adson <paulo.alves200581@gmail.com>
2024-05-13 22:03:14 -0300

    entendendo o que acontece ao tentar concatenar um numero e uma letra

---
+++ java-anatomia-classes/src/estudo/paulo/operadores/concatenacaoDeNumerosELetras.java
@@ -0,0 +1,29 @@
+package estudo.paulo.operadores;
+
+public class concatenacaoDeNumerosELetras {
+    public static void main(String[] args) {
+        String concatenacao = "?";
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + 1 + 1 + "1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + 1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + "1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + 1 + 1 + 1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + (1 + 1 + 1);
+
+        System.out.println(concatenacao);
+    }
+
+}
