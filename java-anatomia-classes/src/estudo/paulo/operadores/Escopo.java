1fbee9e4b2765ea392d089a5d3de2fb6ea5f3492
Paulo Adson <paulo.alves200581@gmail.com>
2024-05-14 17:55:31 -0300

    aprendendo sobre escopo

---
+++ java-anatomia-classes/src/estudo/paulo/operadores/Escopo.java
@@ -0,0 +1,5 @@
+package estudo.paulo.operadores;
+
+public class Escopo {
+    // O escopo pode ser entendido como,o ambiente onde uma variável pode ser acessada. Em Java, o escopo de variáveis vai de acordo com o bloco onde ela foi declarada.
+}
