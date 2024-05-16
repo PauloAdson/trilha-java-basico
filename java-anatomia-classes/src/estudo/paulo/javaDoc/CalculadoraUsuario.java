9bf0f574265a17bb2bcfd57785971394a766571d
Paulo Adson <paulo.alves200581@gmail.com>
2024-05-15 22:55:44 -0300

    aprendendo sobre documentação

---
+++ java-anatomia-classes/src/estudo/paulo/javaDoc/CalculadoraUsuario.java
@@ -0,0 +1,9 @@
+package estudo.paulo.javaDoc;
+
+public class CalculadoraUsuario {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        
+        System.out.println(calculadora.somar(10, 10));
+    }
+}
---
+++ java-anatomia-classes/src/estudo/paulo/javaDoc/Comentarios.java
@@ -0,0 +1,7 @@
+package estudo.paulo.javaDoc;
+
+public class Comentarios {
+    public static void main(String[] args) {
+        // Olá, eu sou um comentário  em uma única linha
+    }
+}
