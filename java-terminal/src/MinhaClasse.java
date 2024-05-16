0f8425612ea1269829f04f333c3c33e230950790
Paulo Adson <paulo.alves200581@gmail.com>
2024-05-16 00:05:50 -0300

    apanhando para criar uma pasta e aprendendo a executar o java no terminal

---
+++ java-terminal/src/MinhaClasse.java
@@ -0,0 +1,10 @@
+public class MinhaClasse {
+    public static void main(String[] args) {
+        /*
+         * para executar no terminal é necessário pegar o caminho até a pasta bin e
+         * ulizar:
+         * java nomeDaClasseQueEstaNoBin
+         */
+        System.out.println("Oi, fui executado pelo Terminal");
+    }
+}
