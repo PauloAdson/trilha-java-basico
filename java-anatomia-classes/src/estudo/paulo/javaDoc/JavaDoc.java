40f19624254d9cf5a60ba05bb1ee1370804c6729
Paulo Adson <paulo.alves200581@gmail.com>
2024-05-15 23:21:16 -0300

    vendo como faz para crir uma documentação html

---
+++ java-anatomia-classes/src/estudo/paulo/javaDoc/JavaDoc.java
@@ -0,0 +1,9 @@
+package estudo.paulo.javaDoc;
+
+public class JavaDoc {
+
+    // Criando nossa documentação no formato html, para disponibilizar via web.
+    // No terminal execute o comando abaixo
+
+    // javadoc -encoding UTF-8 -docencoding ISO-8859-1 -d ../docs src/*.java
+}
