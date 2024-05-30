513df989ec0cf8caa08db180aae19aec5fcfc3d2
Paulo Adson <paulo.alves200581@gmail.com>
2024-05-30 16:07:22 -0300

    salario funcionario, exercicio

---
+++ DesafiosDeCodigoAperfeicoeSuaLogica/src/SalarioFuncionario.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class SalarioFuncionario {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Salario do funcionario");
+        float salarioBruto = scanner.nextFloat();
+        System.out.println("valor dos benefícios do funcionário");
+        float adcionalBeneficios = scanner.nextFloat();
+        scanner.close();
+        float imposto = 0;
+
+        if (salarioBruto > 0 && salarioBruto <= 1100.00) {
+            imposto = 5;
+        } else if (salarioBruto > 1100.00 && salarioBruto <= 2500.00) {
+            imposto = 10;
+        } else {
+            imposto = 15;
+        }
+
+        float calculoPorcentagem = (imposto / 100) * salarioBruto;
+        imposto = calculoPorcentagem;
+        float salario = salarioBruto - imposto + adcionalBeneficios;
+        System.out.println(salario);
+
+    }
+}
