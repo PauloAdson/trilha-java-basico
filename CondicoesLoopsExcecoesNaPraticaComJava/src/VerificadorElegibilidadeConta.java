d8209ed925553e339db347d183111d06be8a5d69
Paulo Adson <paulo.alves200581@gmail.com>
2024-05-31 00:16:08 -0300

    arrumando a formatação do código

--- CondicoesLoopsExcecoesNaPraticaComJava/src/VerificadorElegibilidadeConta.java
+++ CondicoesLoopsExcecoesNaPraticaComJava/src/VerificadorElegibilidadeConta.java
@@ -7,14 +7,15 @@ public class VerificadorElegibilidadeConta {
 
         int idade = scanner.nextInt();
         if (idade >= 18) {
-          System.out.println("Voce esta elegivel para criar uma conta bancaria.");
+            System.out.println("Voce esta elegivel para criar uma conta bancaria.");
         } else {
-          System.out.println("Voce nao esta elegivel para criar uma conta bancaria.");
+            System.out.println("Voce nao esta elegivel para criar uma conta bancaria.");
         }
 
         // TODO: Verificar se a idade do cliente
         // TODO: Se >= 18, imprimir "Voce esta elegivel para criar uma conta bancaria."
-        // TODO: Caso contrário, imprimir "Voce nao esta elegivel para criar uma conta bancaria."
+        // TODO: Caso contrário, imprimir "Voce nao esta elegivel para criar uma conta
+        // bancaria."
 
         // Fechar o scanner para evitar vazamentos de recursos
         scanner.close();
